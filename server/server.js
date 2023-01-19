9367dec078867ec07c0d027283d5f9b6f5e20bc6
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2023-01-19 13:08:47 +1100

    deployment bug

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // Serves up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/assets/images')));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/'))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
